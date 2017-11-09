cec9be95f4b2943826c0497d54998f394818c5e0
Elizabeth <elizabeth.cooper21@gmail.com>
2017-11-09 13:19:13 -0500

    added add/delete user

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://nfl-schedule-maker.herokuapp.com/testUsersGet',
+        success: function(data) {
+            $.each(data, function(index, value) {
+                console.log(value.username);
+                $('<option/>').val(value.username).html(value.username).appendTo('#delete_user');
+                $('<option/>').val(value.username).html(value.username).appendTo('#reset_password');
+            })
+        }
+    });
     $("#add_user_button").click(function(){
         var data = {};
         data.fName = $("#fName").val();
@@ -18,10 +29,57 @@ $(document).ready(function() {
             success: function(data) {
                 console.log('success');
                 console.log(data);
+                location.reload();
             },
-            error: function(jqXHR, textStatus, err) {
-                //show error message
-                alert('text status '+textStatus+', err '+err)
+            error: function (data) {
+                console.log('error');
+                console.log(data);
+                location.reload();
+            }
+        });
+    });
+    $("#reset_password_button").click(function(){
+        var data = {};
+        data.username = $('#reset_password').find(":selected").text();
+        data.password = "password";
+        $.ajax({
+            type: 'POST',
+            url: 'https://nfl-schedule-maker.herokuapp.com/changePassword',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log('success');
+                console.log(data);
+                location.reload();
+            },
+            error: function (data) {
+                console.log('error');
+                console.log(data);
+                location.reload();
+            }
+        });
+    });
+    $("#delete_user_button").click(function(){
+        var data = {};
+        data.username = $('#delete_user').find(":selected").text();
+        console.log("test");
+        console.log(data);
+        $.ajax({
+            type: 'POST',
+            url: 'https://nfl-schedule-maker.herokuapp.com/deleteUser',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log('success');
+                console.log(data);
+                location.reload();
+            },
+            error: function (data) {
+                console.log('error');
+                console.log(data);
+                location.reload();
             }
         });
     });
